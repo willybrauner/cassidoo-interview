d5db97e021ffc61ae3e6ea856b031d2fc0a41444
Willy Brauner <willybrauner@gmail.com>
2026-07-21 22:20:15 +0200

    add jsdox

--- exercices/2026-07-20-count-blinks/index.ts
+++ exercices/2026-07-20-count-blinks/index.ts
@@ -1,5 +1,10 @@
 /**
- * count Blinks
+ * Given a string of . and _ where _ represents a blink, 
+ * return the longest gap between blinks.
+ * 
+ * ex:
+ * countBlinks("_..__...._.___........_") // 8
+ * 
  */
 export const countBlinks = (pattern: string): number =>
   pattern
