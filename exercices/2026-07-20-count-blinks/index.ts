f01d69fed17d6501996fca60bc1cec6b203693bd
Willy Brauner <willybrauner@gmail.com>
2026-07-21 22:40:57 +0200

    improve

--- exercices/2026-07-20-count-blinks/index.ts
+++ exercices/2026-07-20-count-blinks/index.ts
@@ -8,8 +8,8 @@
  */
 export const countBlinks = (pattern: string): number =>
   pattern
-    .match(new RegExp(/_(\.*)_/g))
+    .match(/_(\.*)_/g)
     ?.filter((e) => e !== "")
     .map((e) => e.slice(1, -1))
-    .sort()
-    .reverse()?.[0]?.length || 0
+    .sort((a, b)=> b.length - a.length)
+    ?.[0]?.length || 0
